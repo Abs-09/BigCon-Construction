e29d156834fb528eac68e85ce3487144c03a7319
absal <absal2009@live.com>
2022-11-06 17:08:11 +0500

    tested remove dhoani remove diesel function

--- src/main/Main.java
+++ src/main/Main.java
@@ -9,68 +9,70 @@ public class Main {
 
         // ADD FUNCTION TESTS --------------------
 
-        // diesel in m3
-        System.out.println("adding 1m3 diesel");
-        Dhoani.addDiesel(1);
-        System.out.println("adding 1m3 diesel");
-        Dhoani.addDiesel(1);
-        System.out.println("adding 1m3 diesel");
-        Dhoani.addDiesel(1);
-        System.out.println("adding 0.5m3 diesel");
-        Dhoani.addDiesel(0.5);
-        System.out.println("adding 1m3 diesel");
-        Dhoani.addDiesel(1);
-        // frozen in kgs
-        System.out.println("adding 10kgs frozen");
-        Dhoani.addFrozen(10);
-        System.out.println("adding 10kgs frozen");
-        Dhoani.addFrozen(10);
-        System.out.println("adding 10kgs frozen");
-        Dhoani.addFrozen(10);
-        System.out.println("adding 5kgs frozen");
-        Dhoani.addFrozen(5);
-        System.out.println("adding 10kgs frozen");
-        Dhoani.addFrozen(10);
-        // Fridge in kgs
-        System.out.println("adding 10kgs fridge");
-        Dhoani.addFridge(10);
-        System.out.println("adding 10kgs fridge");
-        Dhoani.addFridge(10);
-        System.out.println("adding 10kgs fridge");
-        Dhoani.addFridge(10);
-        System.out.println("adding 10kgs fridge");
-        Dhoani.addFridge(10);
-        System.out.println("adding 10kgs fridge");
-        Dhoani.addFridge(10);
-        System.out.println("adding 10kgs fridge");
-        Dhoani.addFridge(10);
-        // adding shared materials in kgs(food, protected, unprotected)
-        System.out.println("adding 2000kgs food");
-        Dhoani.addFood(2000);
-        System.out.println("adding 3000 kgs protected");
-        Dhoani.addProtectedMaterials(3000);
-        System.out.println("adding 5000 kgs unprotected");
-        Dhoani.addUnprotectedMaterials(5000);
-        System.out.println("adding 2000kgs food");
-        Dhoani.addFood(2000);
-        System.out.println("adding 3000 kgs protected");
-        Dhoani.addProtectedMaterials(3000);
-        System.out.println("adding 5000 kgs unprotected");
-        Dhoani.addUnprotectedMaterials(5000);
-        System.out.println("adding 2000kgs food");
-        Dhoani.addFood(2000);
-        System.out.println("adding 3000 kgs protected");
-        Dhoani.addProtectedMaterials(3000);
-        System.out.println("adding 5000 kgs unprotected");
-        Dhoani.addUnprotectedMaterials(5000);
-        System.out.println("adding 2000kgs food");
-        Dhoani.addFood(2000);
-        System.out.println("adding 3000 kgs protected");
-        Dhoani.addProtectedMaterials(3000);
-        System.out.println("adding 5000 kgs unprotected");
-        Dhoani.addUnprotectedMaterials(5000);
-        System.out.println("adding 500kgs food");
-        Dhoani.addFood(500);
+        // // diesel in m3
+        // System.out.println("adding 1m3 diesel");
+        // Dhoani.addDiesel(1);
+        // System.out.println("adding 1m3 diesel");
+        // Dhoani.addDiesel(1);
+        // System.out.println("adding 1m3 diesel");
+        // Dhoani.addDiesel(1);
+        // System.out.println("adding 0.5m3 diesel");
+        // Dhoani.addDiesel(0.5);
+        // System.out.println("adding 1m3 diesel");
+        // Dhoani.addDiesel(1);
+        // // frozen in kgs
+        // System.out.println("adding 10kgs frozen");
+        // Dhoani.addFrozen(10);
+        // System.out.println("adding 10kgs frozen");
+        // Dhoani.addFrozen(10);
+        // System.out.println("adding 10kgs frozen");
+        // Dhoani.addFrozen(10);
+        // System.out.println("adding 5kgs frozen");
+        // Dhoani.addFrozen(5);
+        // System.out.println("adding 10kgs frozen");
+        // Dhoani.addFrozen(10);
+        // // Fridge in kgs
+        // System.out.println("adding 10kgs fridge");
+        // Dhoani.addFridge(10);
+        // System.out.println("adding 10kgs fridge");
+        // Dhoani.addFridge(10);
+        // System.out.println("adding 10kgs fridge");
+        // Dhoani.addFridge(10);
+        // System.out.println("adding 10kgs fridge");
+        // Dhoani.addFridge(10);
+        // System.out.println("adding 10kgs fridge");
+        // Dhoani.addFridge(10);
+        // System.out.println("adding 10kgs fridge");
+        // Dhoani.addFridge(10);
+        // // adding shared materials in kgs(food, protected, unprotected)
+        // System.out.println("adding 2000kgs food");
+        // Dhoani.addFood(2000);
+        // System.out.println("adding 3000 kgs protected");
+        // Dhoani.addProtectedMaterials(3000);
+        // System.out.println("adding 5000 kgs unprotected");
+        // Dhoani.addUnprotectedMaterials(5000);
+        // System.out.println("adding 2000kgs food");
+        // Dhoani.addFood(2000);
+        // System.out.println("adding 3000 kgs protected");
+        // Dhoani.addProtectedMaterials(3000);
+        // System.out.println("adding 5000 kgs unprotected");
+        // Dhoani.addUnprotectedMaterials(5000);
+        // System.out.println("adding 2000kgs food");
+        // Dhoani.addFood(2000);
+        // System.out.println("adding 3000 kgs protected");
+        // Dhoani.addProtectedMaterials(3000);
+        // System.out.println("adding 5000 kgs unprotected");
+        // Dhoani.addUnprotectedMaterials(5000);
+        // System.out.println("adding 2000kgs food");
+        // Dhoani.addFood(2000);
+        // System.out.println("adding 3000 kgs protected");
+        // Dhoani.addProtectedMaterials(3000);
+        // System.out.println("adding 5000 kgs unprotected");
+        // Dhoani.addUnprotectedMaterials(5000);
+        // System.out.println("adding 500kgs food");
+        // Dhoani.addFood(500);
+        Dhoani.addDiesel(3);
+        Dhoani.removeDiesel(1.5);
     }
 
 }
